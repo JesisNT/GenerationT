3d5af345aef7d8902850d9bf7e0ef49022bdf1db
Jesús Santiago Di Lorenzo <117871940+JesusSDL@users.noreply.github.com>
2023-07-05 11:00:52 -0300

    Terminando ejercicio Bonus

--- JS/Clase 19-05/script.js
+++ JS/Clase 19-05/script.js
@@ -1,36 +1,60 @@
 const words = [
-  "californication",
-  "plataforma5",
-  "black",
-  "summer",
-  "flea",
-  "aeroplane",
-  "peppers",
-  "unlimited",
-  "arcadium",
-  "love",
-  "getaway",
-  "stadium",
-  "quixoticelixer",
-  "quarter",
-  "snow",
-  "dylan",
-  "zephyr",
-  "funky",
-  "chili",
-];
+    "californication",
+    "plataforma5",
+    "black",
+    "summer",
+    "flea",
+    "aeroplane",
+    "peppers",
+    "unlimited",
+    "arcadium",
+    "love",
+    "getaway",
+    "stadium",
+    "quixoticelixer",
+    "quarter",
+    "snow",
+    "dylan",
+    "zephyr",
+    "funky",
+    "chili",
+  ];
+  
+      
+  var palabraAleatoria = randomWords();
+  var time = 10;
+  var score = 0;
+  var palabraIngresada; 
+   
+  function randomWords() {
+    randomizador = Math.floor(Math.random() * words.length);
+    return words[randomizador]; 
+}
+randomWords();
 
-    function randomWord() {
-        randomizador = Math.floor(Math.random() * words.length);
-        return words[randomizador]; 
-    }
-
-var palabraAleatoria = randomWord();
-var time = 10;
-var score = 0;
-
-    function addToDOM() {
-    return document.getElementById("randomWord2").innerHTML = randomWord();
+   function addToDOM() {
+    document.getElementById("randomWord2").innerHTML = palabraAleatoria;
     }
 addToDOM();
+
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', recuperaValor);
+
+function recuperaValor(e) {
+ return palabraIngresada = e.srcElement.value;
+}
+
+input.addEventListener('input', comparaValores);
+function comparaValores(){
+    if(palabraIngresada === palabraAleatoria){
+        time += 3;
+        input.innerHTML = "";
+        
+    }
+    else return false;
+}
+
+
 
