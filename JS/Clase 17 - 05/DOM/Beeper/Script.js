e7298fdcb9e0c05f5079439db8ac29b9d3056c3d
Jesús Santiago Di Lorenzo <117871940+JesusSDL@users.noreply.github.com>
2023-07-12 10:34:43 -0300

    actualizando ult ejercicios

--- JS/Clase 17 - 05/DOM/Beeper/Script.js
+++ JS/Clase 17 - 05/DOM/Beeper/Script.js
@@ -4,5 +4,16 @@ function parrafoBeep(){
     var node = document.createElement('p');
         node.appendChild(document.createTextNode("BEEP"));
         document.querySelector('body').appendChild(node);
+        node.style.backgroundColor = colorRandom() ;
+    }
+
+
+function colorRandom(){
+const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const rbgColor = `rgb(${r},${g},${b})`;
+  return rbgColor;
 }
 btnBeep.addEventListener('click', parrafoBeep);
---
+++ JS/Clase 17 - 05/DOM/DivMágico/script.js
@@ -0,0 +1,6 @@
+var mouse  = document.getElementById("contenedor");
+
+mouse.addEventListener(
+    "mouseleave",
+    (event) => {
+      event.target.style.display = "none"});
